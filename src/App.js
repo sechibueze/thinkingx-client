00ca0519feb0d81d195dd1cd2b180a9a596ee627
Samuel Chibueze <sechibueze@gmail.com>
2021-04-30 16:25:15 +0100

    installed and configured graphql client to the server

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  InMemoryCache,
+} from "@apollo/client";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthContextProvider from "./context/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
@@ -5,18 +11,46 @@ import Signup from "./components/SignupForm";
 import Login from "./components/LoginForm";
 import Home from "./pages/Home";
 import ForgotPasswordForm from "./components/ForgotPasswordForm";
+import { GRAPHQL_SERVER_URI } from "./constants";
+// Configure Apollo client
+const client = new ApolloClient({
+  uri: GRAPHQL_SERVER_URI,
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query Todos {
+        todos {
+          title
+          description
+          completed
+          due_date
+        }
+      }
+    `,
+  })
+  .then((result) => console.log("Apollo client connected", result))
+  .catch((error) => console.log("Apollo client failed : ", error));
 function App() {
   return (
-    <Router>
-      <Switch>
-        <AuthContextProvider>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/forgot-password" component={ForgotPasswordForm} />
-        </AuthContextProvider>
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <AuthContextProvider>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route
+              exact
+              path="/forgot-password"
+              component={ForgotPasswordForm}
+            />
+          </AuthContextProvider>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const GRAPHQL_SERVER_URI =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:4000";
